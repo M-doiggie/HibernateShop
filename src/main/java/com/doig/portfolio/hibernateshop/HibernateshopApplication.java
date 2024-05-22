353c3874993f3dae72aca0663ebfdbd98c833353
Michael Doig <michaeldoig@hotmail.co.uk>
2024-05-22 08:08:03 +0100

    Inital Commit: Spring Initalizr

---
+++ src/main/java/com/doig/portfolio/hibernateshop/HibernateshopApplication.java
@@ -0,0 +1,13 @@
+package com.doig.portfolio.hibernateshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doig/portfolio/hibernateshop/HibernateshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doig.portfolio.hibernateshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
